88b464b39bd617ad1a195cb041236b8aef583f2a
Dave Bonan <55212683+dbonan@users.noreply.github.com>
2022-01-19 20:39:41 -0800

    Add files via upload

---
+++ eos.py
@@ -0,0 +1,29 @@
+def eos(T, S, T0, S0, rho0, alpha, beta):
+    """
+    Description:
+    
+    Computes potential density using a linear equation of state.
+        
+    input: 
+        T    = temperature                            [ °C ]
+        S    = salinity                               [ g/kg ]
+        T0   = reference temperature                  [ °C ]
+        S0   = reference salinity                     [ g/kg ]
+        rho0 = reference density                      [ kg/m^3 ]
+        
+    output:
+        P  = potential density                        [ kg/m^3 ]
+        PT = thermal component of potential density   [ kg/m^3 ]
+        PH = haline component of potential density    [ kg/m^3 ]
+    """
+    
+    # Thermal component 
+    PT = rho0*(alpha*(T-T0)
+    
+    # Haline component 
+    PH = rho0*(beta*(S-S0))
+               
+    # Total
+    P  = rho0 + PT + PH
+    
+    return P, PT, PH
